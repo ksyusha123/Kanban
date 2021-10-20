8305b95b736f29aedb3c57a443e3adc2e7b7c70f
Ksyusha <astakhova.ksyusha.07@mail.ru>
2021-10-20 17:26:22 +0500

    Пустое решение

---
+++ Kanban/Kanban/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kanban
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
