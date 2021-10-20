6784194bd1a3e2d2a0d144ef2d4d0324e2f28078
Evgeny Khoroshavin <themplarer@gmail.com>
2021-10-20 22:47:48 +0500

    copied

---
+++ Kanban/Application/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public interface IRepository<T>
+    {
+        Task<T> GetAsync(Guid id);
+
+        Task AddAsync(T entity);
+
+        Task UpdateAsync(T entity);
+
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ Kanban/Persistence/KanbanDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class KanbanDbContext : DbContext
+    {
+        public KanbanDbContext(DbContextOptions<KanbanDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //todo добавить описание моделей, когда они появятся
+        }
+    }
+}
