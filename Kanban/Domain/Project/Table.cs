0d350c0561d8c2722024ea067515cd54425e1801
Evgeny Khoroshavin <themplarer@gmail.com>
2021-11-15 01:55:55 +0500

    поменял Table

--- Kanban/Domain/Project/Table.cs
+++ Kanban/Domain/Project/Table.cs
@@ -1,38 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace Domain
 {
     public class Table
     {
-        private IEnumerable<ITask> _tasks = new List<ITask>();
-        private IEnumerable<IExecutor> _teamMembers = new List<IExecutor>();
-        private Dictionary<IExecutor, AccessRights> _users = new Dictionary<IExecutor, AccessRights>();
-        private IEnumerable<State> _states = new List<State>();
+        private readonly List<ITask> _tasks = new();
+        private readonly Dictionary<IExecutor, AccessRights> _users = new();
+        private readonly List<State> _states = new();
         public IReadOnlyCollection<State> States => _states.ToArray();
         public IReadOnlyCollection<ITask> Tasks => _tasks.ToArray();
-        public IReadOnlyCollection<IExecutor> Team => _teamMembers.ToArray(); // по дефолту есть права на редактирование
+        public IEnumerable<IExecutor> Team => _users.Keys; // по дефолту есть права на редактирование
+        public IEnumerable<IExecutor> Readers => FilterExecutors(AccessRights.Read);
+        public IEnumerable<IExecutor> Editors => FilterExecutors(AccessRights.Edit);
+        public IEnumerable<IExecutor> Admins => FilterExecutors(AccessRights.Admin);
 
-        public void AddTask(ITask task)
-        {
-            _tasks = _tasks.Append(task);
-        }
+        public void AddTask(ITask task) => _tasks.Add(task);
 
-        public void AddTeamMember(IExecutor executor)
-        {
-            _teamMembers = _teamMembers.Append(executor);
-            _users.Add(executor, AccessRights.Edit);
-        }
-
-        public void AddExecutorWithAccessRights(IExecutor executor, AccessRights accessRights)
-        {
+        public void AddExecutor(IExecutor executor, AccessRights accessRights = AccessRights.Read) =>
             _users.Add(executor, accessRights);
-        }
 
-        public void AddState(State state)
-        {
-            _states = _states.Append(state);
-        }
+        public void AddState(State state) => _states.Add(state);
+
+        private IEnumerable<IExecutor> FilterExecutors(AccessRights accessRights) =>
+            _users
+                .Where(p => p.Value == accessRights)
+                .Select(p => p.Key);
     }
 }
