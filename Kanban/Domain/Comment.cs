e7e60ef4e16a930a69786b9a2ce371f6d1369640
Evgeny Khoroshavin <themplarer@gmail.com>
2021-11-26 00:40:52 +0500

    Revert "коммент теперь IEntity!"
    This reverts commit 5c834f03e403c097cb711e4b721ce0c960775349.

--- Kanban/Domain/Comment.cs
+++ Kanban/Domain/Comment.cs
@@ -3,16 +3,14 @@ using Infrastucture;
 
 namespace Domain
 {
-    public class Comment : IEntity
+    public class Comment
     {
-        public Guid Id { get; }
         public IExecutor Author { get; }
         public string Message { get; }
         public DateTime CreationTime { get; }
 
-        public Comment(Guid id, IExecutor author, string message, IDateTimeProvider dateTimeProvider)
+        public Comment(IExecutor author, string message, IDateTimeProvider dateTimeProvider)
         {
-            Id = id;
             Author = author;
             Message = message;
             CreationTime = dateTimeProvider.GetCurrent();
