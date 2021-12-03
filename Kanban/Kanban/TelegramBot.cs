09edc467b6f80b0146636516dc348b165a2b9602
Ksyusha <astakhova.ksyusha.07@mail.ru>
2021-12-03 19:14:44 +0500

    команды добалвяются через рефлексию

--- Kanban/Kanban/TelegramBot.cs
+++ Kanban/Kanban/TelegramBot.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using Microsoft.Extensions.Configuration;
 using Telegram.Bot;
 using Telegram.Bot.Args;
@@ -11,17 +12,12 @@ namespace Kanban
     public class TelegramBot
     {
         private readonly TelegramBotClient _client;
-        private static Dictionary<string, ICommand> _commands = new()
-        {
-            {"/start@AgileBoardBot", new StartCommand()}, 
-            {"/help@AgileBoardBot", new HelpCommand()},
-            {"/start", new StartCommand()},
-            {"/help", new HelpCommand()}
-        };
+        private static Dictionary<string, ICommand> _commands;
 
         public TelegramBot(IConfiguration configuration)
         {
             _client = new TelegramBotClient(configuration.GetSection("botToken").Value);
+            _commands = FillCommandsDictionary();
         }
 
         public void Start()
@@ -40,6 +36,29 @@ namespace Kanban
             {
                 await _commands[command].Execute(message, _client);
             }
+        }
+
+        private static Dictionary<string, ICommand> FillCommandsDictionary()
+        {
+            var dict = new Dictionary<string, ICommand>();
+            var commands = Assembly
+                .GetExecutingAssembly()
+                .GetTypes()
+                .Where(t => t.GetInterfaces().Contains(typeof(ICommand)));
+            foreach (var command in commands)
+            {
+                var commandInstance = (ICommand) Activator.CreateInstance(command);
+                // ReSharper disable PossibleNullReferenceException
+                var commandName = command
+                    .GetProperty("Name")
+                    .GetValue(commandInstance)
+                    .ToString();
+                // ReSharper disable once AssignNullToNotNullAttribute
+                dict[commandName] = commandInstance;
+                dict[$"{commandName}@AgileBoardBot"] = commandInstance;
+            }
+
+            return dict;
         }
     }
 }
