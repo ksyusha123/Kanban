327b5045b087923ae729d8860eb115d3f62842bc
Ivan <krash577388@mail.ru>
2021-12-06 01:27:40 +0500

    TrelloApi v1.2 (new classes and board's methods)

---
+++ TrelloApi/TrelloActionData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrelloApi
+{
+    public class TrelloActionData
+    {
+        public TrelloBoard Board { get; set; }
+        public TrelloList List { get; set; }
+    }
+}
---
+++ TrelloApi/TrelloMember.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrelloApi
+{
+    public class TrelloMember
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public string Username { get; set; }
+    }
+}
