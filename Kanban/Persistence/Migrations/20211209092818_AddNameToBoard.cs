bc0bb7ee3ed586fea3b6df833e330dcc9bdf5a18
Evgeny Khoroshavin <themplarer@gmail.com>
2021-12-09 15:16:16 +0500

    миграция

---
+++ Kanban/Persistence/Migrations/20211209092818_AddNameToBoard.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class AddNameToBoard : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Board",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Board");
+        }
+    }
+}
