d495497d4516242af79b2426fa0205229e279c18
Ivan <krash577388@mail.ru>
2021-12-16 13:18:12 +0500

    tmp

--- TestApi/Program.cs
+++ TestApi/Program.cs
@@ -8,7 +8,6 @@ namespace TestApi
     {
         static void Main(string[] args)
         {
-            TrelloClient.Token = "1610c44f0d446765b29a8459127e82571f128cf9fad01a960c11db55448ac0d5";
             var board = new TrelloBoard("cS6ZHBdL");
             var list = board.GetAllLists().First();
             list.GetAllCards();
